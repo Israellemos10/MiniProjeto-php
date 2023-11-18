a49fde98543dc6d226bf6648bf8ea7febcc14742
Lira1981 <150397164+Lira1981@users.noreply.github.com>
2023-11-18 10:47:37 -0300

    quemsomos.php
    Fiz alterações  no tamanho da fonte do texto <p>

--- quemsomos.php
+++ quemsomos.php
@@ -1,5 +1,15 @@
 <div>
     <h1>Quem somos</h1>
+    <style>
+ .large-text {     
+  font-size: 18px;
+ }
+        </style>
+
+    <div class="large-text">
     <p> Estamos aqui para lhe orientar no seu dia a dia, com dicas de bem estar e saúde</p>
+    </div>
+
+   
 
 </div>   
