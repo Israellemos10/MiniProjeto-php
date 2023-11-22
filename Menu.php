c0e5698f4cf51a6441d45edffa75363e5f9bd668
Lira1981 <150397164+Lira1981@users.noreply.github.com>
2023-11-22 02:06:52 -0300

    Create Menu.php
    No menu.php , foi a cor de verde para a cor branco. Também foi feita uma alteração no tamanho da fonte do nome "menu do site"  para 11px

---
+++ Menu.php
@@ -0,0 +1,24 @@
+<div style= "background-color : #00FF00s">
+<style>
+  
+  a {
+    display: inline;
+    margin-right: 20px;
+  }
+</style>
+<style>
+ .large-text {     
+  font-size: 11px;
+ }
+        </style>
+    <div class="large-text">
+<h2>MENU DO SITE</h2>
+</div>
+
+<div class="left">
+<a href= "index.php"> inicio </a> 
+<a href= "?pg=quemsomos">Quem Somos  </a>   
+<a href= "?pg=clientes"> Clintes </a>   
+<a href= "?pg=faleconosco">Fale Conosco </a> 
+<a href= "?pg=teste">teste </a>
+</div>
