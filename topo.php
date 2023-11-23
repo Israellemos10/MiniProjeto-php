14eb9865f7a985ad6051254c0d5cb7768448953a
Lira1981 <150397164+Lira1981@users.noreply.github.com>
2023-11-23 02:00:51 -0300

    Add files via upload
    /* fiz alteração na frase coloquei um hifen  e aumentei o tamanho da foto de 200 para 850 */

--- topo.php
+++ topo.php
@@ -1,8 +1,7 @@
 <div style= "background-color :  #00FF00S">
 
 
-    <center> <h2> Saúde e Bem estar </h2> <center>
-
-    <img src=https://cevisa.org.br/spa/wp-content/uploads/2021/02/Bem-estar-fi%CC%81sico-e-mental.jpeg  width="200"
+    <center> <h2> Saúde e Bem-estar </h2> <center> 
+    <img src=https://cevisa.org.br/spa/wp-content/uploads/2021/02/Bem-estar-fi%CC%81sico-e-mental.jpeg  width="850"
 
 </div>
