71075729a958a00a96988cfb8e5ce514e74e4ee7
Periclesshenrique <116108019+Periclesshenrique@users.noreply.github.com>
2023-11-23 19:21:54 -0300

    Update rodape.php

--- rodape.php
+++ rodape.php
@@ -3,7 +3,7 @@
 <img src=https://ms-publico.s3-sa-east-1.amazonaws.com/cms/post/2019/11/c_imagens-coaching-gpperformance-saude-bem-estar_a77ed88a66ca830a02dea39fbe015a6e87f942e0.jpg width="500"
 
 <br>
-    <H2>RODAPE DO SITE</H2>
+
 
 
 
